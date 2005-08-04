42404f7e99a33899d0a3f1554ca349fd05114b58
kalahan <kalahan@0c53311b-5dfc-0310-a0e2-a6bdf9fc0dca>
2005-08-04 10:14:26 +0000

    Fixed ugly bug where "Make list" fails marking some words. Those words would be hidden forever. Database tables changed default values for some fields

--- trunk/src/lib/base.sql
+++ trunk/src/lib/base.sql
@@ -1,13 +1,9 @@
--- MySQL dump 8.23
----------------------------------------------------------
--- Server version	3.23.58
-
 --
 -- Table structure for table `lang_german`
 --
 
 CREATE TABLE lang_german (
-  datein datetime NOT NULL default '2001-01-01 00:00:00',
+  datein datetime NOT NULL default '2000-01-01 00:00:00',
   word char(100) NOT NULL default '',
   translation char(100) NOT NULL default '',
   tip_trans char(100) default NULL,
@@ -15,12 +11,13 @@ CREATE TABLE lang_german (
   genre set('masculin','femenin','neutrum') default NULL,
   type char(20) default NULL,
   level smallint(6) NOT NULL default '0',
-  lasthit date NOT NULL default '0000-00-00',
+  lasthit date NOT NULL default '0001-01-01',
   misses int(8) NOT NULL default '0',
   hot tinyint(1) default '0',
   PRIMARY KEY  (word)
 ) TYPE=MyISAM;
 
+--
 --
 -- Table structure for table `types_german`
 --
@@ -29,4 +26,5 @@ CREATE TABLE types_german (
   name char(20) NOT NULL default '',
   PRIMARY KEY  (name)
 ) TYPE=MyISAM;
+
 
--- trunk/src/smarty/templates/study/hardest.tpl
+++ trunk/src/smarty/templates/study/hardest.tpl
@@ -38,7 +38,6 @@
     <td>{$item}</td>
   {/foreach}
 </tr>
-
 {/foreach}
 
 </table></p>
